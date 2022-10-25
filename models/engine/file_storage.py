34e89207d3bb9f06bbbd61f2c7d43ede44fd0855
beryl452 <beryl.houessou@gmail.com>
2022-10-25 18:40:36 +0000

    Add all and new methods in FileStorage class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Definition of the FileStorage class."""
+class FileStorage:
+    """Class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances
+    Args:
+        __file_path (str): Path to the JSON file (ex: file.json)
+        __objects (dict): Empty but will store all objects by <class name>.id
+
+    Methods:
+        all(self): returns the dictionary __objects
+        new(self, obj): sets in __objects the obj with key <obj class name>.id
+        save(self): serializes __objects to the JSON file (path: __file_path)
+        reload(self): deserializes the JSON file to __objects
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects
+        
+        Args:
+            self (FileStorage): The current instance
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id
+        
+        Args:
+            self (FileStorage): The current instance
+            obj (any): Any object
+        """
+        self.__objects["{obj.__class__.__name__}.{obj.id}"]=obj
+
