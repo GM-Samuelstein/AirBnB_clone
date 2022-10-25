9d2bf7a9ea4f70cecb6f121bca78b133be2d070e
beryl452 <beryl.houessou@gmail.com>
2022-10-25 07:16:42 +0000

    BaseModel - Add methods: __str__, save, __init__

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Base class for Airbnb clone project
+    Methods:
+        __init__(self, *args, **kwargs)
+        __str__(self)
+        save(self)
+        to_dict(self)
+    """
+    def __init__(self, *args, **kargs):
+        """Initialize the BaseModel class.
+
+        Args:
+            self (BaseModel): The current instance
+            args (list): Not used here
+            kargs (dict): dictionary of key/value pairs attributes
+        """
+        if kwargs:
+            iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, iso_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+
+    def __str__(self):
+        """Return the string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.today()
