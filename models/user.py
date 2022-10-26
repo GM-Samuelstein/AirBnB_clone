74732ccd426bbf477073a4e802a3cfb50a5060d3
Ibitowa Ifeoluwa <ibitowaifeoluwa@yahoo.com>
2022-10-26 11:13:26 -0700

    Updated user.py

--- models/user.py
+++ models/user.py
@@ -8,8 +8,8 @@ class User(BaseModel):
     """A representation of a user.
 
     Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
+        email (str) : The email of the user.
+        password (str) : The password of the user.
         first_name (str) : The first name of the user.
         last_name (str) : The last name of the user.
     """
