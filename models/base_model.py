48d6df25b77d35d3829ee13e26e6eeb4ac715faa
beryl452 <beryl.houessou@gmail.com>
2022-10-26 07:39:05 +0000

    Update models/__init__.py and models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Definition of the BaseModel class."""
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
     """Base class for Airbnb clone project
@@ -25,11 +26,12 @@ class BaseModel:
                 if key in ["created_at", "updated_at"]:
                     setattr(self, key, datetime.strptime(value, iso_format))
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
+            models.storage.save()
 
     def __str__(self):
         """Return the string representation of the instance."""
@@ -38,6 +40,7 @@ class BaseModel:
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
